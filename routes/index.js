6a72f354d84177e18b956c4aa0b481448d7e4f97
ibraheem sadiq <ibraheemsadiq22@gmail.com>
2019-08-14 15:36:52 +0500

    geotab testing by ibm

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,25 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-/* GET home page. */
+
+/* GET distress page. */
+router.get('/distress', function(req, res, next) {
+  res.render('addin-drive/distress/distress.html', { title: 'Express' });
+});
+
+/* GET navigator page. */
+router.get('/navigator', function(req, res, next) {
+  res.render('addin-drive/navigator/navigator.html', { title: 'Express' });
+});
+
+/* GET map page. */
 router.get('/map', function(req, res, next) {
   res.render('addin-mapreplay/mapreplay.html', { title: 'Express' });
+});
+
+/* GET compliancereport page. */
+router.get('/compliancereport', function(req, res, next) {
+  res.render('compliancereport/src/compliance.html', { title: 'Express' });
 });
 
 module.exports = router;
