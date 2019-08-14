a219c365047fa3789e9e9a4c854141e33b75ac15
ibraheem sadiq <ibraheemsadiq22@gmail.com>
2019-08-14 16:32:22 +0500

    engine error

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.use('/', indexRouter);
 
@@ -34,7 +34,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error.html');
 });
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -12,17 +12,17 @@ router.get('/distress', function(req, res, next) {
 });
 
 /* GET navigator page. */
-router.get('/navigator', function(req, res, next) {
+router.get('/navigator/', function(req, res, next) {
   res.render('addin-drive/navigator/navigator.html', { title: 'Express' });
 });
 
 /* GET map page. */
-router.get('/map', function(req, res, next) {
+router.get('/map/', function(req, res, next) {
   res.render('addin-mapreplay/mapreplay.html', { title: 'Express' });
 });
 
 /* GET compliancereport page. */
-router.get('/compliancereport', function(req, res, next) {
+router.get('/compliancereport/', function(req, res, next) {
   res.render('compliancereport/src/compliance.html', { title: 'Express' });
 });
 
